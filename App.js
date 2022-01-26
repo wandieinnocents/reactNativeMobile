f4a82b3d166503f7652049ee871a7d348ed3d1d7
wandieinnocents <wandieinnocent2@gmail.com>
2022-01-26 09:19:34 -0800

    Add icons on bottom tab navigation

--- App.js
+++ App.js
@@ -7,13 +7,51 @@ import  HomeScreen   from './Screens/HomeScreen';
 import  RequestsScreen   from './Screens/RequestsScreen';
 import  ServiceHistory   from './Screens/ServiceHistory';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+    
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Requests') {
+              iconName = focused 
+              ? 'ios-car'
+                : 'ios-car-outline';
+            }
+
+            else if (route.name === 'History') {
+              iconName = focused 
+              ? 'ios-time'
+                : 'ios-time-outline';
+            }
+            else if (route.name === 'Account') {
+              iconName = focused 
+              ? 'ios-people'
+                : 'ios-people-outline';
+            }
+
+
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+      
+      
         <Tab.Screen name="Home" component={HomeScreen} />
         
         <Tab.Screen
